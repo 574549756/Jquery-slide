e75b5fa6001f2a559d80a246f12dcf896566947e
574549756 <574549756@qq.com>
2018-05-05 22:34:17 +0800

    解决倒放问题，优化代码

--- main.js
+++ main.js
@@ -1,114 +1,79 @@
-let n = 1
-let index = $('.current').index()
-reset()
-
-//计时器
-function setTimer(){
-    return setInterval(()=>{
-        makeLeave(getImage(n))
-        .one('transitionend', (e)=>{
-            makeEnter($(e.currentTarget))
-        })
-        makeCurrent(getImage(n+1))
-        activeButton(getButton(n))
-        n += 1
-    },3000)
-}
+let $slides = $('#slides')
+let $imges = $slides.children('img')
+let current = 0
+let $buttons = $('#buttons > #normal')
 
 
-//点击事件
-$('.left').on('click', function(){
-    var index = $('.current').index()
-    pressEventP1(index)
-    if(index === 0){
-        index = size
-    }
-    pressEventP2(index)
+makeFakeSlides()
+
+$slides.css({transform: 'translateX(-640px)'})
+bindEvents()
+$(right).on('click', function(){
+    gotoSlide(current + 1)
 })
-$('.right').on('click', function(){
-    var index = $('.current').index()    
-    pressEventP1(index)
-    pressEventP2(index+2)
+$(left).on('click', function(){
+    gotoSlide(current - 1)
 })
 
-
-//hover停滞事件
-$('.windowInside,#buttons,.arrow').on('mouseenter', function(){
-    window.clearInterval(timerId)
-})
-$('.windowInside,#buttons,.arrow').on('mouseleave', function(){
-    timerId = setTimer()
-})
-
-
-
-
-
-
-
-
-
-
-
-
-/////////////////////////下面都是工具///////////////////////////
-var allButtons = $('#buttons > div')
-
-var size = $('#images > img').length
-
-function createButton(){
-    for(let i=0;i<size;i++){
-        $('#buttons').append('<div class="normal"></div>');
-    }
-}
-
-var timerId = setTimer()
-
-function x(n){
-    if(n>size){
-        n = n%size
-        if (n===0){
-            n=size
-        }
-    }
-    return n
-}
-function getImage(n){
-    return $(`.images > img:nth-child(${x(n)})`)
-}
-function getButton(n){
-    return $(`#buttons > div:nth-child(${x(n)})`)
-}
-function activeButton($button){
-    return $button
-   .addClass('black').siblings('.black').removeClass('black')
-}
-
-function reset(){
-    n = 1
-    $(`.images > img:nth-child(${n})`).addClass('current')
-    .siblings().addClass('enter')
-}
-function makeCurrent($node){
-    return $node.removeClass('enter leave').addClass('current')
-}
-function makeLeave($node){
-    return $node.removeClass('enter current').addClass('leave')
-}
-function makeEnter($node){
-    return $node.removeClass('leave current').addClass('enter')
-}
-function delateCurrent(n){
-    return $(`.images > img:nth-child(${x(n)})`).siblings().removeClass('current')
-}
-function pressEventP1(index){
-    makeLeave(getImage(index+1))
-    .one('transitionend', (e)=>{
-        makeEnter($(e.currentTarget))
+function bindEvents(){
+    $('#buttons').on('click', '#normal', function(e){
+        let $button = $(e.currentTarget)
+        let index = $button.index()
+        activeButton($button)
+        gotoSlide(index)
     })
 }
-function pressEventP2(index){
-    delateCurrent(index)    
-    makeCurrent(getImage(index))
-    activeButton(getButton(index))
+
+
+let timer = setInterval(function(){
+    gotoSlide(current + 1)
+},2000)
+$('.windowInside, #buttons, .arrow').on('mouseenter', function(){
+    window.clearInterval(timer)
+}).on('mouseleave', function(){
+    timer = setInterval(function(){
+        gotoSlide(current + 1)
+    }, 2000)
+})
+
+function gotoSlide(index){
+    if(index > $buttons.length-1){
+        index = 0
+    }else if(index < 0){
+        index = $buttons.length - 1
+    }
+    console.log('index','current')
+    console.log(index, current)
+    if(current === $buttons.length-1 && index === 0){
+        //最后一张到第一张
+        $slides.css({transform:`translateX(${-($buttons.length+1) * 640}px)`})
+        .one('transitionend', function(){
+            $slides.hide().offset()
+            $slides.css({transform:`translateX(${-(index+1)*640}px)`}).show()
+        })
+    }else if(current === 0 && index === $buttons.length -1){
+        //第一张到最后一张
+        $slides.css({transform:`translateX(0px)`})
+        .one('transitionend', function(){
+            $slides.hide().offset()
+            $slides.css({transform:`translateX(${-(index+1)*640}px)`}).show()
+        })
+    }else{
+        $slides.css({transform: `translateX(${-(index+1) * 640}px)`})
+    }
+    current = index
+    activeButton($('#buttons').children('#normal').eq(index))
+}
+
+
+function activeButton($button){
+    $button.addClass('black').siblings('.black').removeClass('black')
+}
+
+
+function makeFakeSlides(){
+    let $fakeFirst = $imges.eq(0).clone(true)
+    let $fakeLast = $imges.eq($imges.length-1).clone(true)
+    $slides.append($fakeFirst)
+    $slides.prepend($fakeLast)
 }
